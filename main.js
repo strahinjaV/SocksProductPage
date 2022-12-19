2cb0563ecd20f3a167c761ed12269875ff655f50
Strahinja <strahinjav98@gmail.com>
2022-12-19 13:18:14 +0100

    saving my changes to compare with solution

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            link: 'https://socks.com.au/',
         }
     }
 })
