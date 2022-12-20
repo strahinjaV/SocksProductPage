6935dabb3ee3aeb33b29785cc8dc12c6e2800e60
Strahinja <strahinjav98@gmail.com>
2022-12-20 23:29:58 +0100

    my solution

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="decreaseCart">
+          Remove 1 from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,10 +63,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+        this.$emit('add-to-cart',this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
-          this.selectedVariant = index
+        this.selectedVariant = index
+      },
+      decreaseCart(){
+        this.$emit('decrease-cart')
       }
   },
   computed: {
--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id) += 1
+        },
+
+        decrease(){
+            this.cart.pop()
+        }
+    },
 })
